32ff8bde65b62e04f6b8ada80398ef70a79188b0
shopnonirfoundation-svg <shopnonirfoundation@gmail.com>
2025-09-01 00:33:08 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('form').forEach(form => {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Thank you for your message! We will get back to you soon.');
+        this.reset();
+    });
+});
